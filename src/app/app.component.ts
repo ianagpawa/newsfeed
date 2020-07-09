f051faf2d8a5e2642cc4df68b293e87061880c47
ian.agpawa <agpawaji@gmail.com>
2020-07-08 23:39:40 -0400

    feat: Working base

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NewsApiService } from './news-api/news-api.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'newsfeed';
+  articles:Array<any>;
+  sources:Array<any>;
+
+  constructor(private newsapi:NewsApiService){ }
+
+  ngOnInit() {
+      this.newsapi.getSources().subscribe(data => this.sources = data['sources']);
+      this.newsapi.getArticles().subscribe(data=> this.articles = data['articles']);  
+    }
+
+  searchArticles(source){
+    console.log("selected source is: "+source);
+    this.newsapi.getArticlesById(source).subscribe(data => this.articles = data['articles']);
+  }
 }
--- src/app/news-api/news-api.service.ts
+++ src/app/news-api/news-api.service.ts
@@ -5,18 +5,22 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class NewsApiService {
-  api_key: string = null;
+  api_key: string = "7bcd8e5bfb0242a98e83331d98bd9ee6";
   
   constructor(private http:HttpClient) { }
 
-  getSources(): Promise<any> { return this.getRequest('sources?language=en'); }
+  getSources() { return this.getRequest('sources?language=en'); }
   
-  getArticles(): Promise<any> { return this.getRequest('top-headlines?sources=techcrunch'); }
+  getArticles() { return this.getRequest('top-headlines?sources=techcrunch'); }
   
-  getArticlesByID(source: String): Promise<any> { return this.getRequest(`?sources=${source}`); }
+  getArticlesById(source: String) { return this.getRequest(`?sources=${source}`); }
 
   getRequest(path: string) {
     const fullPath: string = `https://newsapi.org/v2/${path}&apiKey=${this.api_key}`;
-    return new Promise( (resolve,reject) => { resolve(this.http.get(fullPath)); });
+    // return new Promise( (resolve,reject) => { resolve(this.http.get(fullPath)); });
+    return this.http.get(fullPath);
   }
+
+  
+  
 }
