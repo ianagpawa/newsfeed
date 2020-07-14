31795b4b5bfffe23ac127cbfdf7d4497aee16073
ian.agpawa <agpawaji@gmail.com>
2020-07-14 00:17:10 -0400

    WIP: Start add formly component

--- src/app/news/header/header.module.ts
+++ src/app/news/header/header.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToolbarModule } from 'primeng/toolbar';
 import { HeaderComponent } from './header.component';
+import { FormlyComponentModule } from './formly/formly.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { HeaderComponent } from './header.component';
   ],
   imports: [
     BrowserAnimationsModule,
-    ToolbarModule
+    ToolbarModule,
+    FormlyComponentModule
   ],
   providers: [ ],
   bootstrap: [ HeaderComponent ],
