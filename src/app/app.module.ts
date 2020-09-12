817eb7ce7e0d6c8ccdab0d31d67a91cddd6e31ce
Ian Agpawa <agpawaji@gmail.com>
2020-09-12 17:05:34 -0400

    start migration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { NewsModule } from './news/news.module';
 
 @NgModule({
   declarations: [ AppComponent ],
   imports: [
-    BrowserModule,
-    NewsModule
+    BrowserModule
   ],
   providers: [ ],
   bootstrap: [ AppComponent ]
